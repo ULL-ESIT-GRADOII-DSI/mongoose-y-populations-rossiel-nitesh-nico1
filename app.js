bdc715cbc73a26ed9a5a1d10f0f0f8c1d65e36ab
alu0100763478 <alu0100763478@ull.edu.es>
2016-05-09 12:57:40 +0000

    Primera version

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const expressLayouts = require('express-ejs-layouts');
 
 const calculate = require('./models/calculate.js');
 const basedatos = require('./models/schema.js');
+const Person = basedatos.personSchema;
+const Story = basedatos.storySchema;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -27,18 +29,55 @@ app.get('/csv', (request, response) => {
   
 });
 
+//Cuando se introduce un nombre de usuario
+
+app.get('/usuario', (request, response) => {
+    /* ... Consultar la base de datos y retornar contenidos de usuario si existe... */
+    
+    console.log(basedatos.personSchema);
+    
+      Person.find({name: request.params.usuario},
+            function(err,data) {
+                if(err)
+                {
+                    console.error("Se ha producido un error");
+                }
+                else
+                {
+                    console.log("Enviando datos a csv.js => Id de usuario:"+data[0]._id);
+                    const id = mongoose.Types.ObjectId(data[0]._id);
+                    
+                    Story.find({_creator: id},
+                        function(err,data_tablas) {
+                            if(err)
+                            {
+                                console.error("Se ha producido un error->"+err);
+                            }
+                            else
+                            {
+                                console.log("Enviando dattos a csv.js => Tablas asociadas:"+data_tablas);    
+                            }
+                            response.send({contenido: data_tablas, usuario_propietario: id});
+                    });
+                }
+    });
+    
+    /* ... si no existe, crear usuario con una colección de imputs vacía... */
+});
+
 //Cuando se guarda el nombre
 
 app.get('/mongo/:nombre', function(req, res) {
   /* ... Consultar la base de datos y retornar contenidos de input1 ... */
-  basedatos.find({}, function(err, docs) {
+  basedatos.CsvSchema.find({}, function(err, docs) {
         if (err)
             return err;
         if (docs.length >= 4) {
-            basedatos.remove({ id: docs[3].id }).exec();
+            basedatos.CsvSchema.remove({ id: docs[3].id }).exec();
         }
   });
-        let c4 = new basedatos({
+        //este let se hace en el schema (no se puede crear una variable nueva así) [mirar como se ha de hacer]
+        let c4 = new basedatos.CsvSchema({
         "id": req.params.nombre,
         "data": req.query.dataString
         });
@@ -53,7 +92,7 @@ app.get('/mongo/:nombre', function(req, res) {
 
 
 app.get('/encuentra', function(req, res) {
-    basedatos.find({}, function(err, docs) {
+    basedatos.csv.find({}, function(err, docs) {
         if (err)
             return err;
         res.send(docs);
@@ -63,7 +102,7 @@ app.get('/encuentra', function(req, res) {
 /*Se devuelve como respuesta la entrada correspondiente al nombre
   especificado en la request*/
 app.get('/imput', function(req, res) {
-    basedatos.find({
+    basedatos.csv.find({
         id: req.query.id
     }, function(err, docs) {
         res.send(docs);
--- public/csv.js
+++ public/csv.js
@@ -1,36 +1,8 @@
 // See http://en.wikipedia.org/wiki/Comma-separated_values
 (() => {
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-
-const resultTemplate = `
-<div class="contenido">
-      <table class="center" id="result">
-          <% _.each(rows, (row) => { %>
-          <tr class="<%=row.type%>">
-              <% _.each(row.items, (name) =>{ %>
-              <td><%= name %></td>
-              <% }); %>
-          </tr>
-          <% }); %>
-      </table>
-  </p>
-</div>
-`;
-
-/* Volcar la tabla con el resultado en el HTML */
-const fillTable = (data) => { 
-  $("#finaltable").html(_.template(resultTemplate, { rows: data.rows })); 
-};
-
-/* Volcar en la textarea de entrada 
- * #original el contenido del fichero fileName */
-const dump = (fileName) => {
-  $.get(fileName, function (data) {
-      $("#original").val(data);
-  });
-};
  
-const handleFileSelect = (evt) => {
+/*const handleFileSelect = (evt) => {
   evt.stopPropagation();
   evt.preventDefault();
 
@@ -44,7 +16,7 @@ const handleFileSelect = (evt) => {
   reader.readAsText(files[0])
 }
 
-/* Drag and drop: el fichero arrastrado se vuelca en la textarea de entrada */
+/* Drag and drop: el fichero arrastrado se vuelca en la textarea de entrada 
 const handleDragFileSelect = (evt) => {
   evt.stopPropagation();
   evt.preventDefault();
@@ -68,15 +40,25 @@ const handleDragOver = (evt) => {
 
 
 
-
+*/
 
 $(document).ready(() => {
     let original = document.getElementById("original");  
     if (window.localStorage && localStorage.original) {
       original.value = localStorage.original;
     }
+    
+    /* Request AJAX para que se busque el usuario */
+    
+    $("#usuario").click( () => {
+        if (window.localStorage) localStorage.user = user.value;
+      
+        $.get("/usuario", 
+          { user: user.value }
+        );
+   });    
 
-    /* Request AJAX para que se calcule la tabla */
+    /* Request AJAX para que se calcule la tabla 
     $("#parse").click( () => {
         if (window.localStorage) localStorage.original = original.value;
       
@@ -98,7 +80,7 @@ $(document).ready(() => {
         });
     });
     
-   /* botones para rellenar el textarea */
+   /* botones para rellenar el textarea 
     $('button.example').each( (_,y) => {
          console.log($(y).text());
 
@@ -110,8 +92,6 @@ $(document).ready(() => {
             $("#original").val(data[0].data);
         });
        });
-       
-//       dump(`${$(y).text()}.txt`); });
    });
    
     $.get("/encuentra", {}, (data) => {
@@ -121,7 +101,6 @@ $(document).ready(() => {
             if (data[i]) {
                 $('button.example').get(i).className = "example";
                 $('button.example').get(i).textContent = data[i].id;
-            //    $('drop_zone').get(i).textContent = data[i].data;
             }
         }
     });
@@ -132,7 +111,7 @@ $(document).ready(() => {
     dropZone.addEventListener('dragover', handleDragOver, false);
     dropZone.addEventListener('drop', handleDragFileSelect, false);
     let inputFile = $('.inputfile')[0];
-    inputFile.addEventListener('change', handleFileSelect, false);
+    inputFile.addEventListener('change', handleFileSelect, false);*/
  });
 })();
 
